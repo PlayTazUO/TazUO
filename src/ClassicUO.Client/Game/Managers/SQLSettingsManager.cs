944aaf148b86fcc3b1d028c4ff3d6f84a343d295
TazmanianTad <bittiez@gmail.com>
2025-10-05 07:43:00 -0600

    Update SQLSettingsManager.cs

--- src/ClassicUO.Client/Game/Managers/SQLSettingsManager.cs
+++ src/ClassicUO.Client/Game/Managers/SQLSettingsManager.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using ClassicUO.Configuration;
-using ClassicUO.LegionScripting;
 using ClassicUO.Utility.Logging;
 using Microsoft.Data.Sqlite;
 
@@ -65,7 +64,7 @@ namespace ClassicUO.Game.Managers
                                                  PRIMARY KEY (scope, name)
                                              )
                                              """;
-                await createTableCmd.ExecuteNonQueryAsync();
+                await createTableCmd.ExecuteNonQueryAsync().ConfigureAwait(false);
 
                 // Create index for faster lookups
                 await using SqliteCommand createIndexCmd = connection.CreateCommand();
@@ -132,6 +131,41 @@ namespace ClassicUO.Game.Managers
             }
         }
 
+        private T ParseValue<T>(string value, T defaultValue)
+        {
+            if (string.IsNullOrEmpty(value))
+                return defaultValue;
+
+            try
+            {
+                Type type = typeof(T);
+
+                if (type == typeof(string))
+                    return (T)(object)value;
+
+                if (type == typeof(bool))
+                    return (T)(object)bool.Parse(value);
+
+                if (type == typeof(int))
+                    return (T)(object)int.Parse(value);
+
+                if (type == typeof(uint))
+                    return (T)(object)uint.Parse(value);
+
+                if (type == typeof(short))
+                    return (T)(object)short.Parse(value);
+
+                if (type == typeof(ushort))
+                    return (T)(object)ushort.Parse(value);
+
+                return defaultValue;
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
         private string GetScopeKey(SettingsScope scope)
         {
             switch (scope)
@@ -153,21 +187,84 @@ namespace ClassicUO.Game.Managers
             }
         }
 
+        /// <summary>
+        /// Synchronously retrieves a setting value from the database.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist</param>
+        /// <returns>The setting value or the default value if not found</returns>
         public string Get(SettingsScope scope, string name, string defaultValue = "")
         {
             return GetAsync(scope, name, defaultValue).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
-        public string Get(SettingsScope scope, string name, string defaultValue, Action<string> onComplete)
+        /// <summary>
+        /// Synchronously retrieves a strongly-typed setting value from the database.
+        /// </summary>
+        /// <typeparam name="T">The type to parse the setting value to (string, int, uint, short, ushort, bool)</typeparam>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist or parsing fails</param>
+        /// <returns>The parsed setting value or the default value if not found or parsing fails</returns>
+        public T Get<T>(SettingsScope scope, string name, T defaultValue = default)
         {
-            return GetAsync(scope, name, defaultValue, onComplete).ConfigureAwait(false).GetAwaiter().GetResult();
+            return GetAsync(scope, name, defaultValue).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Asynchronously retrieves a setting value from the database.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist</param>
+        /// <returns>A task that represents the asynchronous operation, containing the setting value or the default value if not found</returns>
         public async Task<string> GetAsync(SettingsScope scope, string name, string defaultValue = "")
         {
             return await GetAsync(scope, name, defaultValue, null);
         }
 
+        /// <summary>
+        /// Asynchronously retrieves a strongly-typed setting value from the database.
+        /// </summary>
+        /// <typeparam name="T">The type to parse the setting value to (string, int, uint, short, ushort, bool)</typeparam>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist or parsing fails</param>
+        /// <returns>A task that represents the asynchronous operation, containing the parsed setting value or the default value if not found or parsing fails</returns>
+        public async Task<T> GetAsync<T>(SettingsScope scope, string name, T defaultValue = default)
+        {
+            string stringValue = await GetAsync(scope, name, defaultValue?.ToString() ?? string.Empty);
+            return ParseValue(stringValue, defaultValue);
+        }
+
+        /// <summary>
+        /// Asynchronously retrieves a strongly-typed setting value and invokes a callback on the main thread.
+        /// </summary>
+        /// <typeparam name="T">The type to parse the setting value to (string, int, uint, short, ushort, bool)</typeparam>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist or parsing fails</param>
+        /// <param name="onComplete">Callback invoked with the parsed result on the main thread</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public async Task GetAsyncOnMainThread<T>(SettingsScope scope, string name, T defaultValue, Action<T> onComplete)
+        {
+            T result = await GetAsync(scope, name, defaultValue);
+            if (onComplete != null)
+            {
+                MainThreadQueue.EnqueueAction(() => onComplete(result));
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously retrieves a setting value without pausing the thread to wait for the results.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="defaultValue">The default value to return if the setting doesn't exist</param>
+        /// <param name="onComplete">Callback invoked with the result. WARNING: Does not run on the main thread! Use MainThreadQueue if needed.</param>
+        /// <returns>A task that represents the asynchronous operation, containing the setting value or the default value if not found</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the manager has been disposed</exception>
         public async Task<string> GetAsync(SettingsScope scope, string name, string defaultValue, Action<string> onComplete)
         {
             if (_disposed)
@@ -208,11 +305,37 @@ namespace ClassicUO.Game.Managers
             }
         }
 
+        /// <summary>
+        /// Synchronously sets a setting value in the database.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="value">The value to store</param>
         public void Set(SettingsScope scope, string name, string value)
         {
             SetAsync(scope, name, value).ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Synchronously sets a strongly-typed setting value in the database.
+        /// </summary>
+        /// <typeparam name="T">The type of the value (string, int, uint, short, ushort, bool)</typeparam>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="value">The value to store</param>
+        public void Set<T>(SettingsScope scope, string name, T value)
+        {
+            SetAsync(scope, name, value).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asynchronously sets a setting value in the database, inserting or replacing as needed.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="value">The value to store</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the manager has been disposed</exception>
         public async Task SetAsync(SettingsScope scope, string name, string value)
         {
             if (_disposed)
@@ -227,9 +350,10 @@ namespace ClassicUO.Game.Managers
                 await connection.OpenAsync().ConfigureAwait(false);
 
                 SqliteCommand cmd = connection.CreateCommand();
-                cmd.CommandText = @"
-                        INSERT OR REPLACE INTO settings (scope, name, value)
-                        VALUES ($scope, $name, $value)";
+                cmd.CommandText = """
+                                  INSERT OR REPLACE INTO settings (scope, name, value)
+                                  VALUES ($scope, $name, $value)
+                                  """;
                 cmd.Parameters.AddWithValue("$scope", scopeKey);
                 cmd.Parameters.AddWithValue("$name", name);
                 cmd.Parameters.AddWithValue("$value", value ?? string.Empty);
@@ -246,6 +370,27 @@ namespace ClassicUO.Game.Managers
             }
         }
 
+        /// <summary>
+        /// Asynchronously sets a strongly-typed setting value in the database, inserting or replacing as needed.
+        /// </summary>
+        /// <typeparam name="T">The type of the value (string, int, uint, short, ushort, bool)</typeparam>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <param name="name">The name of the setting</param>
+        /// <param name="value">The value to store</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the manager has been disposed</exception>
+        public async Task SetAsync<T>(SettingsScope scope, string name, T value)
+        {
+            string stringValue = value?.ToString() ?? string.Empty;
+            await SetAsync(scope, name, stringValue);
+        }
+
+        /// <summary>
+        /// Asynchronously retrieves all settings for a given scope.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <returns>A task that represents the asynchronous operation, containing a dictionary of all setting name-value pairs for the scope</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the manager has been disposed</exception>
         public async Task<Dictionary<string, string>> GetAllAsync(SettingsScope scope)
         {
             if (_disposed)
@@ -261,9 +406,10 @@ namespace ClassicUO.Game.Managers
                 await connection.OpenAsync();
 
                 SqliteCommand cmd = connection.CreateCommand();
-                cmd.CommandText = @"
-                        SELECT name, value FROM settings
-                        WHERE scope = $scope";
+                cmd.CommandText = """
+                                  SELECT name, value FROM settings
+                                  WHERE scope = $scope
+                                  """;
                 cmd.Parameters.AddWithValue("$scope", scopeKey);
 
                 await using SqliteDataReader reader = await cmd.ExecuteReaderAsync();
@@ -285,11 +431,19 @@ namespace ClassicUO.Game.Managers
             }
         }
 
+        /// <summary>
+        /// Synchronously retrieves all settings for a given scope.
+        /// </summary>
+        /// <param name="scope">The settings scope (Char, Account, Server, or Global)</param>
+        /// <returns>A dictionary of all setting name-value pairs for the scope</returns>
         public Dictionary<string, string> GetAll(SettingsScope scope)
         {
             return GetAllAsync(scope).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Releases resources used by the SQLSettingsManager.
+        /// </summary>
         public void Dispose()
         {
             if (_disposed)
