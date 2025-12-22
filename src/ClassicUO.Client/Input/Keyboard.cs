c754e749ad384532956319d1e9211e2a98dd3d7e
jma251 <88570009+jma251@users.noreply.github.com>
2025-12-21 19:20:19 -0600

    Update Keyboard.cs
    Possible Drag Select Fix

--- src/ClassicUO.Client/Input/Keyboard.cs
+++ src/ClassicUO.Client/Input/Keyboard.cs
@@ -2,26 +2,21 @@
 
 // Copyright (c) 2021, andreakarasho
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. All advertising materials mentioning features or use of this software
-//    must display the following acknowledgement:
-//    This product includes software developed by andreakarasho - https://github.com/andreakarasho
-// 4. Neither the name of the copyright holder nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 // LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
@@ -46,9 +41,27 @@ namespace ClassicUO.Input
         public static bool Ctrl { get; private set; }
 
 
+        public static void RefreshModifiers()
+        {
+            // Pull current modifier state directly from SDL (more reliable during mouse-drag selection).
+            SDL.SDL_Keymod mod = SDL.SDL_GetModState() & ~IgnoreKeyMod;
+
+            // AltGr workaround: some keyboards report RALT + LCTRL.
+            if ((mod & (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL)) ==
+                (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL))
+            {
+                mod = SDL.SDL_Keymod.KMOD_NONE;
+            }
+
+            Shift = (mod & SDL.SDL_Keymod.KMOD_SHIFT) != SDL.SDL_Keymod.KMOD_NONE;
+            Alt   = (mod & SDL.SDL_Keymod.KMOD_ALT)   != SDL.SDL_Keymod.KMOD_NONE;
+            Ctrl  = (mod & SDL.SDL_Keymod.KMOD_CTRL)  != SDL.SDL_Keymod.KMOD_NONE;
+        }
+
+
         //public static bool IsKeyPressed(SDL.SDL_Keycode code)
         //{
-        //    return code != SDL.SDL_Keycode.SDLK_UNKNOWN && _code == code;
+        //    return code == _code;
         //}
 
         //public static bool IsModPressed(SDL.SDL_Keymod mod, SDL.SDL_Keymod tocheck)
@@ -89,10 +102,7 @@ namespace ClassicUO.Input
             Alt = (e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != SDL.SDL_Keymod.KMOD_NONE;
             Ctrl = (e.keysym.mod & SDL.SDL_Keymod.KMOD_CTRL) != SDL.SDL_Keymod.KMOD_NONE;
 
-            if (e.keysym.sym != SDL.SDL_Keycode.SDLK_UNKNOWN)
-            {
-                _code = e.keysym.sym;
-            }
+            _code = e.keysym.sym;
         }
     }
 }
