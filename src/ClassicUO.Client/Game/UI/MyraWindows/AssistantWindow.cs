1268f5f8fd0c3178027f0ee645ff604ac9c64153
TazmanianTad <bittiez@gmail.com>
2026-02-22 18:23:17 -0700

    Create a simpler tab control, move general tab to seperate files

--- src/ClassicUO.Client/Game/UI/MyraWindows/AssistantWindow.cs
+++ src/ClassicUO.Client/Game/UI/MyraWindows/AssistantWindow.cs
@@ -1,54 +1,24 @@
 using ClassicUO.Game.UI.Controls;
+using ClassicUO.Game.UI.MyraWindows.Widgets;
 using ClassicUO.Game.UI.MyraWindows.Widgets.Assistant;
-using ClassicUO.Utility;
-using Myra.Graphics2D.UI;
 
 namespace ClassicUO.Game.UI.MyraWindows;
 
 public class AssistantWindow : MyraControl
 {
     public const int WIDTH = 550;
-    private TabItem _selectedTab;
 
     public AssistantWindow() : base("Legion Assistant")
     {
         Build();
-
         CenterInViewPort();
     }
 
     private void Build()
     {
-        var tabs = new TabControl()
-        {
-            Width = WIDTH
-        };
-
-        tabs.SelectedIndexChanged += (s, e) =>
-        {
-            _selectedTab = tabs.SelectedItem;
-
-            if (_selectedTab.Content != null) return;
-
-            if (_selectedTab.Tag is int tPage)
-                switch ((TabPage)tPage)
-                {
-                    default:
-                    case TabPage.General:
-                        _selectedTab.Content = GeneralTab.Build();
-                        break;
-                }
-        };
-
-        tabs.Items.Add(new TabItem("General") { Tag = TabPage.General.ToInt() });
-
-        tabs.SelectedIndex = 0;
-
+        var tabs = new MyraTabControl { Width = WIDTH };
+        tabs.AddTab("General", GeneralTab.Build);
+        tabs.SelectFirst();
         SetRootContent(tabs);
-    }
-
-    private enum TabPage
-    {
-        General
     }
 }
