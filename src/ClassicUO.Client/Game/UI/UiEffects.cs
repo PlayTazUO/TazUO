0a602ed7920b7c496475568a5395afb5bf8d5463
yuval-po <yuval.pomer@protonmail.com>
2026-06-12 18:52:10 +0300

    UI Effects cleanup

--- src/ClassicUO.Client/Game/UI/UiEffects.cs
+++ src/ClassicUO.Client/Game/UI/UiEffects.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Numerics;
 using System.Threading.Tasks;
+using ClassicUO.Common;
 using ClassicUO.Configuration;
 using ClassicUO.Game.Managers;
 using Myra.Graphics2D.UI;
@@ -19,53 +21,126 @@ public static class UiEffects
         if (replacedChildIndex < 0 || replacedChildIndex >= parent.Widgets.Count)
             throw new ArgumentOutOfRangeException(nameof(replacedChildIndex));
 
-        int fadeoutTime = transitionTimeMs / 2;
-        int fadeinTime = transitionTimeMs - fadeoutTime;
-
         // Roughly try to match monitor refresh rate
         int iterationTime = 1000 / Math.Min(GameController.SupportedRefreshRate, Settings.GlobalSettings.FPS);
 
-        int fadeoutIterations = fadeoutTime / iterationTime;
-        int fadeinIterations = fadeinTime / iterationTime;
+        int transitionIterations = transitionTimeMs / 2 / iterationTime;
 
-        // Fade out old
-        Widget oldWidget = parent.Widgets[replacedChildIndex];
-        float opacityDecrements = oldWidget.Opacity / fadeoutIterations;
+        await WidgetRemovalEffect(
+            parent,
+            replacedChildIndex,
+            w => new Accessor<float>(() => w.Opacity),
+            0,
+            1,
+            transitionIterations,
+            false,
+            iterationTime
+        );
 
-        float originalOpacity = oldWidget.Opacity;
-        for (int i = 1; i < fadeoutIterations + 1; i++)
-        {
-            float newOpacity = Math.Max(0, originalOpacity - opacityDecrements * i);
-            MainThreadQueue.InvokeOnMainThread(() => oldWidget.Opacity = newOpacity);
-            if (newOpacity <= 0)
-                break;
-            await Task.Delay(iterationTime);
-        }
+        await WidgetInsertEffect(
+            parent,
+            newWidget,
+            replacedChildIndex,
+            w => new Accessor<float>(() => w.Opacity),
+            0,
+            1,
+            transitionIterations,
+            true,
+            iterationTime
+        );
+    }
 
-        // Fade in new
-        float opacityIncrements = newWidget.Opacity / fadeinIterations;
+    private static async Task WidgetRemovalEffect<TValueType>(
+        Container parent,
+        int widgetIndex,
+        Func<Widget, Accessor<TValueType>> getAffectedProp,
+        TValueType? minValue,
+        TValueType? maxValue,
+        int effectIterations,
+        bool isIncrement,
+        int iterationTimeMs
+    ) where TValueType :
+        struct,
+        INumber<TValueType>
+    {
+        Widget widget = parent.Widgets[widgetIndex];
+
+        Accessor<TValueType> propAccessor = getAffectedProp(widget);
+        TValueType oldPropValue = propAccessor.Get();
+        await WidgetEffect(propAccessor, minValue, maxValue, effectIterations, isIncrement, iterationTimeMs);
 
         // Myra is buggy and doesn't actually handle 'Replace' events...
-        parent.Widgets.RemoveAt(replacedChildIndex);
+        parent.Widgets.RemoveAt(widgetIndex);
 
-        // Once we've removed the widget, we need to restore its original opacity;
-        // otherwise, next render of the same instance will be incorrect
-        oldWidget.Opacity = originalOpacity;
+        // Restore the prop's original value. This localizes the changes to our scope and prevents leaking
+        // effect transitions outside to the consumers
+        propAccessor.Set(oldPropValue);
+    }
 
-        // Store our 'target' opacity
-        originalOpacity = newWidget.Opacity;
+    private static async Task WidgetInsertEffect<TValueType>(
+        Container parent,
+        Widget widget,
+        int widgetIndex,
+        Func<Widget, Accessor<TValueType>> getAffectedProp,
+        TValueType? minValue,
+        TValueType? maxValue,
+        int effectIterations,
+        bool isIncrement,
+        int iterationTimeMs
+    ) where TValueType :
+        struct,
+        INumber<TValueType>
+    {
+        Accessor<TValueType> propAccessor = getAffectedProp(widget);
+        TValueType oldPropValue = propAccessor.Get();
+        await WidgetEffect(propAccessor, minValue, maxValue, effectIterations, isIncrement, iterationTimeMs);
 
-        // Start of complete transparent
-        newWidget.Opacity = 0;
-        parent.Widgets.Insert(replacedChildIndex, newWidget);
+        // Myra is buggy and doesn't actually handle 'Replace' events...
+        parent.Widgets.Insert(widgetIndex, widget);
 
-        for (int i = 1; i < fadeinIterations + 1; i++)
+        // Restore the prop's original value. This localizes the changes to our scope and prevents leaking
+        // effect transitions outside to the consumers
+        propAccessor.Set(oldPropValue);
+    }
+
+    private static async Task WidgetEffect<TValueType>(
+        Accessor<TValueType> affectedProp,
+        TValueType? minValue,
+        TValueType? maxValue,
+        int effectIterations,
+        bool isIncrement,
+        int iterationTimeMs
+    ) where TValueType :
+        struct,
+        INumber<TValueType>
+    {
+        TValueType originalPropValue = affectedProp.Get();
+        TValueType propDiffPerIteration = affectedProp.Get() / TValueType.CreateChecked(effectIterations);
+
+        for (int i = 1; i < effectIterations + 1; i++)
         {
-            float newOpacity = Math.Clamp(opacityIncrements * i, 0, Math.Min(1, originalOpacity));
-            MainThreadQueue.InvokeOnMainThread(() => newWidget.Opacity = newOpacity);
-            if (newOpacity >= 1)
+            TValueType increment = propDiffPerIteration * TValueType.CreateChecked(i);
+            TValueType newPropValue = isIncrement
+                ? originalPropValue + increment
+                : originalPropValue - increment;
+
+            bool breakEarly = false;
+            if (newPropValue < minValue)
+            {
+                newPropValue = minValue.Value;
+                breakEarly = true;
+            }
+            else if (newPropValue > maxValue)
+            {
+                newPropValue = maxValue.Value;
+                breakEarly = true;
+            }
+
+            MainThreadQueue.InvokeOnMainThread(() => affectedProp.Set(newPropValue));
+            if (breakEarly)
                 break;
-            await Task.Delay(iterationTime);
+
+            await Task.Delay(iterationTimeMs);
         }
     }
 }
