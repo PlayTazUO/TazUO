e1042fb226648fe72bd59ab96adb1c78605ce785
Tad Taylor <bittiez@gmail.com>
2026-06-18 22:57:38 -0600

    Add Hue View tab to Tinkerer window (#553)

--- src/ClassicUO.Client/Game/UI/MyraWindows/Widgets/Assistant/Tinkerer/TinkererTab.cs
+++ src/ClassicUO.Client/Game/UI/MyraWindows/Widgets/Assistant/Tinkerer/TinkererTab.cs
@@ -8,6 +8,7 @@ public static class TinkererTab
     {
         var tabs = new MyraTabControl();
         tabs.AddTab("Clilocs", CililocsTabContent.Build);
+        tabs.AddTab("Hues", HueViewTabContent.Build);
         tabs.SelectFirst();
         return tabs;
     }
