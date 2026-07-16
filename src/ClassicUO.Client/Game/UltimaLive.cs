f22250a9ae97f4089eb3febcf19770f3ee94045d
Senzaiken <86119547+Senzaiken@users.noreply.github.com>
2026-07-16 00:55:14 -0400

    Reload dynamic maps only on facet size change and resync the world so… (#628)

--- src/ClassicUO.Client/Game/UltimaLive.cs
+++ src/ClassicUO.Client/Game/UltimaLive.cs
@@ -323,6 +323,11 @@ namespace ClassicUO.Game
                     p.Seek(14);
                     byte mapCount = p.ReadUInt8();
 
+                    if (p.Length < 15 + mapCount * 5)
+                    {
+                        return;
+                    }
+
                     var defs = new (int index, int width, int height)[mapCount];
 
                     for (int i = 0; i < mapCount; i++)
@@ -334,9 +339,22 @@ namespace ClassicUO.Game
                         defs[i] = (index, width, height);
                     }
 
-                    Client.Game.UO.FileManager.Maps.ApplyServerMapDefinitions(defs);
+                    var maps = Client.Game.UO.FileManager.Maps;
 
-                    if (world != null && world.InGame)
+                    int activeMap = (world != null && world.InGame) ? world.MapIndex : -1;
+                    int beforeW = 0, beforeH = 0;
+
+                    if (activeMap >= 0 && activeMap < maps.MapBlocksSize.GetLength(0))
+                    {
+                        beforeW = maps.MapBlocksSize[activeMap, 0];
+                        beforeH = maps.MapBlocksSize[activeMap, 1];
+                    }
+
+                    maps.ApplyServerMapDefinitions(defs);
+
+                    if (activeMap >= 0 &&
+                        (activeMap >= maps.MapBlocksSize.GetLength(0) ||
+                         maps.MapBlocksSize[activeMap, 0] != beforeW || maps.MapBlocksSize[activeMap, 1] != beforeH))
                     {
                         world.ReloadCurrentMap();
                     }
