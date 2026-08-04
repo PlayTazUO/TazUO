605f9c744a0aaadc7ce2d83c26db7f5c6e954170
Tad Taylor <bittiez@gmail.com>
2026-08-03 22:55:48 -0600

    Fix ObjectDisposedException when script API is disposed during execution

--- src/ClassicUO.Client/LegionScripting/ApiClasses/ApiUiGump.cs
+++ src/ClassicUO.Client/LegionScripting/ApiClasses/ApiUiGump.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using ClassicUO.Assets;
 using ClassicUO.Game;
 using ClassicUO.Game.Managers;
@@ -14,8 +15,10 @@ namespace ClassicUO.LegionScripting.ApiClasses;
 
 public class ApiUiGump(LegionAPI api)
 {
-        private T OnMain<T>(Func<T> func) => MainThreadQueue.InvokeOnMainThread(func, api.CancellationToken.Token);
-        private void OnMain(Action action) => MainThreadQueue.InvokeOnMainThread(action, api.CancellationToken.Token);
+        private readonly CancellationToken _cachedToken = api.CancellationToken.Token;
+
+        private T OnMain<T>(Func<T> func) => MainThreadQueue.InvokeOnMainThread(func, _cachedToken);
+        private void OnMain(Action action) => MainThreadQueue.InvokeOnMainThread(action, _cachedToken);
 
     /// <summary>
     /// Get a blank gump.
