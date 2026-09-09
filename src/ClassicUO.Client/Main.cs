81315e9d24c2ed85c85e631acd2087cfeaa6b27a
LasherasGH <lasherasdeveloper@gmail.com>
2026-09-09 09:37:39 +0200

    Added support for forcing DirectX 11 with ForceDriver 4. It improves performance in general on some older Windows machines.

--- src/ClassicUO.Client/Main.cs
+++ src/ClassicUO.Client/Main.cs
@@ -224,6 +224,10 @@ namespace ClassicUO
 
                     case 3: // SDL/FNA auto-select
                         break;
+                    case 4: // DirectX 11
+                        Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "D3D11");
+                        SDL.SDL_SetHint(SDL.SDL_HINT_RENDER_DRIVER, "direct3d11");
+                        break;
                 }
 
                 Client.Run(pluginHost);
